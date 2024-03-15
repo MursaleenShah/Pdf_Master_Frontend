62df9a0883dcc2444e97441e6181aa5e0871fa51
MursaleenShah <shahmursalin9@gmail.com>
2024-03-15 15:45:27 +0530

    Updated Form Functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [title,setTitle] = useState("");
+  const [file, setFile] = useState("");
+  const submitImage = async(e)=>{
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title",title);
+    formData.append("file",file);
+    console.log(title,file);
+  }
   return (
     <div className="App">
-       <form className='formStyle'>
+       <form className="formStyle" onSubmit={submitImage}>
         <h4>PDF MASTER</h4>
         <br/>
         <input
@@ -12,6 +22,7 @@ function App() {
         className='form-control'
         placeholder='Title'
         required 
+        onChange={(e)=>setTitle(e.target.value)}
         />
         <br/>
          <input
@@ -19,8 +30,13 @@ function App() {
         className='form-control'
         accept='appliaction/pdf'
         required 
+        onChange={(e)=>setFile(e.target.files[0])}
         />
         <br/>
+        <button className='btn btn-primary'
+        type='submit'>
+          Submit
+        </button>
       </form>
     </div>
   );
