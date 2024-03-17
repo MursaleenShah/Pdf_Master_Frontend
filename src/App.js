58b35d298434e3862859724b337bd588c8c0b344
MursaleenShah <shahmursalin9@gmail.com>
2024-03-17 07:13:09 +0530

    created a componet to show pdf using react-pdf

--- src/App.js
+++ src/App.js
@@ -1,87 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { pdfjs } from "react-pdf";
+import PdfComp from "./components/PdfComp";
 
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
 
 function App() {
-  const [title,setTitle] = useState("");
+  const [title, setTitle] = useState("");
   const [file, setFile] = useState("");
-  const [allImage,setAllImage] = useState(null);
+  const [allImage, setAllImage] = useState(null);
+  const [pdfFile,setPdfFile] =useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     getPdf();
-  },[]);
+  }, []);
 
-//function to get pdf from server
-const getPdf = async() =>{
-  const result = await axios.get("http://localhost:5000/get-files");
-  console.log(result.data.data);
-  setAllImage(result.data.data);
+  //function to get pdf from server
+  const getPdf = async () => {
+    const result = await axios.get("http://localhost:5000/get-files");
+    console.log(result.data.data);
+    setAllImage(result.data.data);
+  };
 
-}
-
-
-
-  const submitImage = async(e)=>{
+  const submitImage = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("title",title);
-    formData.append("file",file);
-    console.log(title,file);
-    const result = await axios.post("http://localhost:5000/upload-files",
-    formData,
-    {
-      headers :{"Content-Type":"multipart/form-data"},
-    });
+    formData.append("title", title);
+    formData.append("file", file);
+    console.log(title, file);
+    const result = await axios.post(
+      "http://localhost:5000/upload-files",
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
     console.log(result);
-     if(result.data.status == "ok"){
+    if (result.data.status === "ok") {
       alert("uploaded successfully!");
-     }
-  }
+      getPdf();
+    }
+  };
+  //function to show pdf on homepage
+  const showPdf = (pdf) => {
+    setPdfFile(`http://localhost:5000/files/${pdf}`)
+  };
   return (
     <div className="App">
-       <form className="formStyle" onSubmit={submitImage}>
+      <form className="formStyle" onSubmit={submitImage}>
         <h4>PDF MASTER</h4>
-        <br/>
+        <br />
         <input
-        type='text'
-        className='form-control'
-        placeholder='Title'
-        required 
-        onChange={(e)=>setTitle(e.target.value)}
+          type="text"
+          className="form-control"
+          placeholder="Title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
         />
-        <br/>
-         <input
-        type='file'
-        className='form-control'
-        accept='appliaction/pdf'
-        required 
-        onChange={(e)=>setFile(e.target.files[0])}
+        <br />
+        <input
+          type="file"
+          className="form-control"
+          accept="appliaction/pdf"
+          required
+          onChange={(e) => setFile(e.target.files[0])}
         />
-        <br/>
-        <button className='btn btn-primary'
-        type='submit'>
+        <br />
+        <button className="btn btn-primary" type="submit">
           Submit
         </button>
       </form>
-      <div className='uploaded'>
+      <div className="uploaded">
         <h4>Uploaded PDF:</h4>
-        <div className='output-div'>
-          {allImage == null 
-          ? "" 
-          : allImage.map((data)=>{
-            return(
-              <div className='inner-div'>
-            <h6>Title:{data.title}</h6>
-            <button className='btn btn-primary'>Show Pdf</button>
-          </div>
-            );
-          })
-          }
+        <div className="output-div">
+          {allImage == null
+            ? ""
+            : allImage.map((data) => {
+                return (
+                  <div className="inner-div">
+                    <h6>Title:{data.title}</h6>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => showPdf(data.pdf)}
+                    >
+                      Show Pdf
+                    </button>
+                  </div>
+                );
+              })}
         </div>
-
       </div>
+      <PdfComp  pdfFile ={pdfFile}/>
     </div>
   );
 }
