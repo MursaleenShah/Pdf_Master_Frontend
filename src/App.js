fd8c9c5f167c45b277b570562a57a6c0893de4e6
MursaleenShah <shahmursalin9@gmail.com>
2024-03-21 08:20:31 +0530

    updated state of form fields

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
 import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { pdfjs } from "react-pdf";
 import PdfComp from "./components/PdfComp";
+import { useRef } from "react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -11,6 +11,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 ).toString();
 
 function App() {
+  const fileInputRef = useRef(null);
   const [title, setTitle] = useState("");
   const [file, setFile] = useState("");
   const [allImage, setAllImage] = useState(null);
@@ -31,7 +32,7 @@ function App() {
 
   const submitImage = async (e) => {
     e.preventDefault();
- 
+
     // Check if file is not null and is a PDF
     if (file && file.type !== "application/pdf") {
       alert("Please select only PDF files.");
@@ -41,8 +42,8 @@ function App() {
     const formData = new FormData();
     formData.append("title", title);
     formData.append("file", file);
-    formData.append("pageNumbers",pageNumbers)
-    console.log(title, file,pageNumbers);
+    formData.append("pageNumbers", pageNumbers);
+    console.log(title, file, pageNumbers);
     const result = await axios.post(
       "http://localhost:5000/upload-files",
       formData,
@@ -54,43 +55,38 @@ function App() {
     if (result.data.status === "ok") {
       alert("uploaded successfully!");
       getPdf();
+      //clear form fields
+      setTitle("");
+      setPageNumbers("");
+      fileInputRef.current.value = null;
+    setFile(null);
     }
   };
   //function to show pdf on homepage
   const showPdf = (pdf) => {
     setPdfFile(`http://localhost:5000/files/${pdf}`);
-  }
+  };
   //function to download modified pdf
   const downloadModifiedPdf = async (pdfId) => {
     try {
-      const response = await axios.get(`http://localhost:5000/get-modified-pdf/${pdfId}`, {
-        responseType: 'blob'
-      });
-      
+      const response = await axios.get(
+        `http://localhost:5000/get-modified-pdf/${pdfId}`,
+        {
+          responseType: "blob",
+        }
+      );
+
       const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       link.href = url;
-      link.setAttribute('download', 'modified_pdf.pdf');
+      link.setAttribute("download", "modified_pdf.pdf");
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
     } catch (error) {
-      console.error('Error downloading modified PDF:', error);
+      console.error("Error downloading modified PDF:", error);
     }
   };
-
-
-
-
-
-
-
-
-
-  
-
-
-
 
   return (
     <div className="App">
@@ -103,6 +99,7 @@ function App() {
           placeholder="Title"
           required
           onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
         <br />
         <input
@@ -111,6 +108,8 @@ function App() {
           accept="appliaction/pdf"
           required
           onChange={(e) => setFile(e.target.files[0])}
+          ref={fileInputRef}
+          
         />
         <br />
         <input
@@ -118,6 +117,7 @@ function App() {
           className="form-control"
           placeholder="Page Numbers to Extract (e.g., 1, 3, 5-7)"
           onChange={(e) => setPageNumbers(e.target.value)}
+          value={pageNumbers}
         />
         <br />
 
@@ -140,10 +140,11 @@ function App() {
                     >
                       Show Pdf
                     </button>
-                    <button className="btn btn-primary"
-                    onClick={() => downloadModifiedPdf(data._id)}>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => downloadModifiedPdf(data._id)}
+                    >
                       Download Pdf
-
                     </button>
                   </div>
                 );
@@ -156,7 +157,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
